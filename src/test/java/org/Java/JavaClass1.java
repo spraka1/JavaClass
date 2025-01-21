8ef62ee45d69c21248db709383a35b652db7c886
spraka1 <sprakashbtech1992@gmail.com>
2025-01-21 14:10:59 +0530

    21/01/2024 changes

--- src/test/java/org/Java/JavaClass1.java
+++ src/test/java/org/Java/JavaClass1.java
@@ -1,5 +1,34 @@
 package org.Java;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class JavaClass1 {
 
+	
+	public static void main(String[] args) {
+		
+		
+		List <Integer> list = new ArrayList <Integer>();
+		list.add(125);
+		list.add(545);
+		list.add(100);
+		list.add(799);
+		list.add(666);
+		list.add(666);
+		list.add(125);
+		list.add(277);
+		
+		System.out.println(list);
+		Collections.sort(list);
+		System.out.println(list);
+		
+		Set <Integer> set = new TreeSet <Integer>(list);
+		
+		System.out.println(set);
+	}
 }
